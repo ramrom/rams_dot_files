4130e0a1c40cb01c606a71f10e8a1ff12519cec9
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2017-08-26 12:40:32 -0500

    update setup new box

--- scripts/setupnewbox.sh
+++ scripts/setupnewbox.sh
@@ -1,16 +1,16 @@
 cd ~
 cp rams_dot_files/.bashrc .
 cp rams_dot_files/.bash_profile .
-cp rams_dot_files/.gitconfig .
-cp rams_dot_files/.git_completion.sh .
-cp rams_dot_files/.vimrc .
-cp -r rams_dot_files/.vim .
-cp rams_dot_files/.tmux.conf .
 ln -s rams_dot_files/.bash_aliases
 ln -s rams_dot_files/.bash_functions
+cp rams_dot_files/.git_completion.sh .
+ln -s rams_dot_files/.vim
+ln -s rams_dot_files/.vimrc
 ln -s rams_dot_files/.psqlrc
+ln -s rams_dot_files/.tmux.conf
+ln -s rams_dot_files/.gitconfig
 
 cp rams_dot_files/scripts/base_tmux_env.sh .
 
 sudo apt-get update
-sudo apt-get -y install tmux tree ack-grep htop exuberant-ctags
+sudo apt-get -y install vim tmux tree ack-grep htop exuberant-ctags
