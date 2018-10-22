205c950a81e62b94ab219a017b3bd4cc5f7e3e49
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2018-10-22 16:48:20 -0500

    update yelp go query

--- scripts/yelp.go
+++ scripts/yelp.go
@@ -17,6 +17,19 @@ type credentials struct {
   ClientID  string `json:"client_id"`
 }
 
+type BizQueryResponse  struct {
+  Businesses []Business `json:"businesses"`
+  Total      int        `json:"total"`
+}
+
+type Business struct {
+  //ID        string    `json:"id"`
+  Alias     string    `json:"alias"`
+  Name      string    `json:"name"`
+  IsClosed  bool      `json:"is_closed"`
+  Rating    float64   `json:"rating"`
+}
+
 func get_yelp_creds() {
   usr, _ := user.Current()
   cred_file, err := ioutil.ReadFile(usr.HomeDir + "/.creds/yelp_api.json")
@@ -29,9 +42,10 @@ func init() {
   get_yelp_creds()
 }
 
-func make_yelp_query() {
+func QueryYelpBusinesses() (*BizQueryResponse, error) {
   //raw_query := fmt.Sprintf("latitude=%v&longitude=%v",41.9033958, -87.642861)
-  raw_query := url.Values{"latitude":{"41.9033958"}, "longitude":{"-87.642861"}}
+  // NOTE: api limits to 50 max, anything above returns no results
+  raw_query := url.Values{"latitude":{"41.9033958"}, "longitude":{"-87.642861"}, "limit":{"50"}}
   u := url.URL{
       Scheme:     "https",
       Host:       "api.yelp.com",
@@ -47,13 +61,26 @@ func make_yelp_query() {
   if err != nil { panic(fmt.Sprintf("failed yelp fusion api call!:  %v",err)) }
   defer res.Body.Close()
   body, err := ioutil.ReadAll(res.Body)
-  //fmt.Println(string(body))
+  //pp_json_res(body)
 
+  var yelpres BizQueryResponse
+  err = json.Unmarshal(body, &yelpres)
+  if err != nil {
+    return nil, fmt.Errorf("portfolio query failed: %v", err)
+  } else {
+    fmt.Println("num businesses returned:",len(yelpres.Businesses))
+    for _, biz := range yelpres.Businesses { fmt.Println(biz.Name) } // just print names
+    //fmt.Println(yelpres)
+    return &yelpres, nil
+  }
+}
+
+func pp_json_res(json_string []byte) {
   var ppjson bytes.Buffer
-  err = json.Indent(&ppjson, body, "", "\t")
+  err := json.Indent(&ppjson, json_string, "", "\t")
   if err != nil { fmt.Println("JSON parse error: ", err) } else { fmt.Println(string(ppjson.Bytes())) }
 }
 
 func main() {
-  make_yelp_query()
+  QueryYelpBusinesses()
 }
