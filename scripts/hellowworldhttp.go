5d82aca376e24d5b4ea23694cb1544200edd8783
Sreeram Mittapalli <smittapalli@enovafinancial.com>
2019-02-10 23:36:35 -0600

    add logging to go hello http

--- scripts/hellowworldhttp.go
+++ scripts/hellowworldhttp.go
@@ -3,13 +3,14 @@ package main
 import (
   "fmt"
   "net/http"
+  "log"
 )
 
 var port string = "8080"
 
 func main() {
-  http.Handle("/json", http.HandlerFunc(myjson))
-  http.Handle("/web", http.HandlerFunc(web))
+  http.Handle("/json", mwlog(auth(http.HandlerFunc(myjson))))
+  http.Handle("/web", mwlog(http.HandlerFunc(web)))
   fmt.Printf("starting server on %v\n", port)
   // generate cert with opensll: https://www.linode.com/docs/security/ssl/create-a-self-signed-tls-certificate/
   err := http.ListenAndServeTLS(fmt.Sprintf(":%v",port),"MyCertificate.crt","MyKey.key",nil)
@@ -28,4 +29,16 @@ func web(w http.ResponseWriter,r *http.Request) {
   w.Header().Set("Content-Type","text/html")
   html := fmt.Sprintf(`<html><body><h1>Werd</h1><p style="color:blue">Your IP: %v</p></body></html>`,r.RemoteAddr)
   w.Write([]byte(html))
+}
+
+func mwlog(handler http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    log.Println(r)
+    handler.ServeHTTP(w,r)
+  })
+}
+func auth(handler http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    handler.ServeHTTP(w,r)
+  })
 }
