1157483429a3e2b26dcedb157f7c12e2628999b4
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2019-05-05 23:34:03 -0500

    tmux status line change, gitconfig add diff is green

--- scripts/bash_color.sh
+++ scripts/bash_color.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+black=`tput setaf 0`
 red=`tput setaf 1`
 green=`tput setaf 2`
 yellow=`tput setaf 3`
@@ -8,6 +9,9 @@ cyan=`tput setaf 6`
 white=`tput setaf 7`
 blue_on_yellow=`tput setaf 4; tput setab 3`
 bold=`tput bold`
+dim=`tput dim`
+und=`tput smul`
+remove_und=`tput rmul`
 reset=`tput sgr0`
 
 echo "${yellow}${bold}BOLD AND YELLOW...${reset}"
