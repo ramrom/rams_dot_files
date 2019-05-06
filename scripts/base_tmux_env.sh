ca2739e96e89d33fa2de617d12410ce7d9aba68a
Sreeram Mittapalli <sreeram.mittapalli@rallyhealth.com>
2019-05-06 16:21:35 -0500

    update base tmux env script

--- scripts/base_tmux_env.sh
+++ scripts/base_tmux_env.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 tmux new-session -d -s baseenv
  
-tmux new-window -t baseenv:0 -n 'admin'
+tmux split-window -h
 tmux new-window -t baseenv:1 -n 'admin'
+tmux split-window -h
 tmux new-window -t baseenv:2 -n 'admin'
+tmux split-window -h
 tmux new-window -t baseenv:3 -n 'admin'
+tmux split-window -h
 tmux new-window -t baseenv:4 -n 'admin'
+tmux split-window -h
+tmux new-window -t baseenv:5 -n 'logs'
+tmux split-window -v
+tmux split-window -h
 
 tmux select-window -t baseenv:0
 tmux -2 attach-session -t baseenv
