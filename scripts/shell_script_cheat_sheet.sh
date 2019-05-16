12533fddae14e65f0bcf8998ff1c418b742a0761
Sreeram Mittapalli <sreeram.mittapalli@rallyhealth.com>
2019-05-16 11:26:35 -0500

    cheatsheet more

--- scripts/shell_script_cheat_sheet.sh
+++ scripts/shell_script_cheat_sheet.sh
@@ -18,3 +18,16 @@ S=${A:1:${#A}-1}  # will print "fooba", omitted the last char
 
 # printf for may newlines
 printf "${A}\n\n"
+
+# from https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream, perl does not remove the tab/indent formatting
+echo "$(tput setaf 3)yellow" | perl -pe 's/\x1b\[[0-9;]*[mG]//g'
+
+# awk script to colorize
+tail -f $1 | awk \
+    -v reset=$(tput sgr0) -v yel=$(tput setaf 3)
+    {f=0}
+    (/Join/ && f==0) {print yel $0 reset;f=1; fflush()}   # fflush force flush to stdout, important if this is piped into another cmd
+    /HTTPie/ {print bblue $0 reset;f=1; fflush()}
+    /io\.Abs[a-z]*C/ {print mag bold $0 reset;f=1; fflush()}
+    f==0 { print $0; fflush() }
+'
