0481fc24a4016a0aaff50a4368af5d2074eda5fc
Sreeram <sreeram.mittapalli@gmail.com>
2019-07-20 16:04:33 -0500

    ram tmux env

--- scripts/ram_tmux_env.sh
+++ scripts/ram_tmux_env.sh
@@ -2,16 +2,21 @@
 
 # horsize=`tput cols`    # how to obtain width/columns of terminal window/pane
 # vertsize=`tput lines`   # how to obtain height/rows of terminal window/pane
+SESSION_NAME=ram
+GDRIVE=~/"Google Drive"
 
-tmux new-session -d -s ram
+tmux new-session -d -s $SESSION_NAME
 tmux select-pane -T 'admin'
+tmux split-window -h -c "$GDRIVE"
+tmux select-pane -T 'admin'
+
+tmux new-window -t $SESSION_NAME:1 -n 'admin' -c "$GDRIVE"
 tmux split-window -h
 tmux select-pane -T 'admin'
 
-tmux new-window -t ram:1 -n 'admin'
+tmux new-window -t $SESSION_NAME:2 -n 'admin' -c "$GDRIVE"
 tmux split-window -h
 tmux select-pane -T 'admin'
 
-tmux new-window -t ram:2 -n 'admin'
-tmux split-window -h
-tmux select-pane -T 'admin'
+tmux select-window -t $SESSION_NAME:0
+tmux -2 attach-session -t $SESSION_NAME
