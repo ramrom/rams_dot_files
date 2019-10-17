19c7ef638b7732aba2a288202c56de876bdc6a19
Sreeram <sreeram.mittapalli@gmail.com>
2019-10-17 15:49:37 -0500

    mongo ram tools thisgetter

--- mongo/.mongorc.js
+++ mongo/.mongorc.js
@@ -1,18 +1,13 @@
 host = db.serverStatus().host;
 prompt = function() { return db+"@"+host+"$ "; }
 
-function thisgetter(cmd_name, getterfunc) {
-    //if (typeof(Object.getOwnPropertyDescriptor(this, cmd_name)) == 'undefined') { Object.defineProperty(this, cmd_name, { get: getterfunc }) }
-    Object.defineProperty(this, cmd_name, { get: getterfunc, configurable: true })
-}
-
 // function sc() { printjson(db.getCollectionNames()); }
-thisgetter("sc", function() { printjson(db.getCollectionNames()); })
+Object.defineProperty(this, "sc", { get: function() { printjson(db.getCollectionNames()) }, configurable: true })
 
 //function sd() { printjson(db.adminCommand({ listDatabases: 1 })) }  //NOTE: cant define foo prop on Object and func foo()
-thisgetter("sd", function() { printjson(db.adminCommand({ listDatabases: 1 })) })
+Object.defineProperty(this, "sd", { get: function() { printjson(db.adminCommand({ listDatabases: 1 })) }, configurable: true })
 
-thisgetter('sdh', function() { printjson(db.getMongo().getDatabaseNames()) }) // mongohacker way
+Object.defineProperty(this, "sdh", { get: function() { printjson(db.getMongo().getDatabaseNames()) }, configurable: true }) // mongohacker way
 
 function ldrallytools() { load('/Users/sreeram.mittapalli/code/rally_ram_dot_files/db_queries/mongo_rally_tools.js'); }
 function ldramtools() { load('/Users/sreeram.mittapalli/rams_dot_files/mongo/mongo_ram_tools.js'); }
