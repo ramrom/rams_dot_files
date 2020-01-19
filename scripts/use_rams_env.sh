52ec96d9e461a8ea3d72cc82c0f4b684e1c17a3b
Sreeram <sreeram.mittapalli@gmail.com>
2020-01-19 01:14:33 -0600

    more cleanup

--- scripts/use_rams_env.sh
+++
@@ -1,6 +0,0 @@
-cd ~
-source RamsEnovaEnvFiles/.bashrc
-source RamsEnovaEnvFiles/.bash_aliases
-source RamsEnovaEnvFiles/.bash_functions
-source RamsEnovaEnvFiles/.bash_profile
-alias vi='vim -u /home/cnuapp/RamsEnovaEnvFiles/.vimrc'
