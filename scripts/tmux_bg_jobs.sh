2471260088e4c82c1140b09ce0bbdf793cee5a0c
Sreeram <sreeram.mittapalli@gmail.com>
2020-01-21 14:52:12 -0600

    cheatsheet, comment

--- scripts/tmux_bg_jobs.sh
+++ scripts/tmux_bg_jobs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# NOTE: i can source other script files, like funcs, and verify they are defined but typeset -F and zsh print -f
+# to count funcs shows zero..., can count aliases correctly
+
 pane_pids=$(tmux list-panes -F "#{pane_pid}")
 for pane_pid in $pane_pids; do
     # -P returns child PIDs of given PID, pgrep returns 1 exit code if no children found
