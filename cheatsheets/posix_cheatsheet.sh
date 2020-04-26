fc134cb626ea72f7d28d105eccd9b00a0d02262e
Sreeram <sreeram.mittapalli@gmail.com>
2020-04-25 20:25:03 -0500

    create posix cheatsheet

---
+++ cheatsheets/posix_cheatsheet.sh
@@ -0,0 +1,10 @@
+# Posix type stuff
+
+# list all files recursively in dir and their disk usage
+du -a /foo
+
+# find a file recursive starting with current dir
+find . -type f -name "*pattern*"
+
+# get terminal info
+infocmp
