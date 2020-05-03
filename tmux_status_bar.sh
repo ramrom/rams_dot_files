f1cdcf7a215f299d9e7175bc71dbbb51167a26d4
Sreeram <sreeram.mittapalli@gmail.com>
2020-05-03 16:30:34 -0500

    more tmux status stuff

--- tmux_status_bar.sh
+++ tmux_status_bar.sh
@@ -2,6 +2,9 @@
 
 # setopt NO_NOMATCH  #needed if this was zsh
 
+# TODO: tput cols x lines with tput reports 80 x 25, the default, in reality i have it set to 135
+# tmux set status-format[1] "#[align=left,fg=red]#(tput cols; tput lines)"
+
 #TODO: separate tmux funcs into seperate script of add here
 source ~/rams_dot_files/shell_functions.sh
 
@@ -13,13 +16,14 @@ source ~/rams_dot_files/shell_functions.sh
 counter=$(tmux show -v @status-bar-interval-counter)
 tmux set -q @status-bar-interval-counter $(( ($counter + 1) % 2520 ))
 
-# foo=$(tmux_render_timer_bar eggs)
-# tmux set status-format[1] "$foo"
+foo=$(tmux_render_timer_bar eggs)
+cpu=$(tmux_percent_usage_color $(cpu_usage))
+tmux set status-format[1] "$foo    CPU-Usage:$cpu"
 
-if [ $(( $counter % 2 )) -eq 0 ]; then
-    tmux set status-format[1] "$(color=200 tmux_render_timer_bar eggs)"
-    # tmux set -a status-format[2] "*"
-fi
+# if [ $(( $counter % 2 )) -eq 0 ]; then
+#     tmux set status-format[1] "$(color=200 tmux_render_timer_bar eggs)"
+#     # tmux set -a status-format[2] "*"
+# fi
 
 # if [ $(( $counter % 4 )) -eq 0 ]; then
 #     tmux set -a status-format[3] "*"
