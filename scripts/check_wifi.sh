c857aba032add33cd02e92a0c04425e09af332f0
Sreeram <sreeram.mittapalli@gmail.com>
2020-08-01 15:05:47 -0500

    add wifi check script for tmux

---
+++ scripts/check_wifi.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# setup alert, prolly tmux status, for not connected to Rally wifi:
+function get_wifi_ssid() {
+    if [ "$(uname)" = "Darwin" ]; then
+        local wifi_info=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I)
+        echo "$wifi_info" | awk -F: '/ SSID/{print $2}' | sed 's/ //g'
+    fi
+}
+
+laptop_wifi_ssid=$(get_wifi_ssid)
+
+[ "$laptop_wifi_ssid" = "jaala" ] && echo "BADWIFI"       # should use 5g
+echo "$laptop_wifi_ssid" | grep "uest" && echo "BADWIFI"  # if on Guest network
