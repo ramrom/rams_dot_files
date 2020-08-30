9193316dec2ad4fc51850518f8a27359daeef473
Sreeram <sreeram.mittapalli@gmail.com>
2020-08-30 10:44:49 -0500

    cheats

--- cheatsheets/ssh_cheatsheet.sh
+++ cheatsheets/ssh_cheatsheet.sh
@@ -1,6 +1,9 @@
 ################### SSH ####################
 
+# ssh-agent can remember the passphrase of a private key, if the key has a passphrase, which is a good idea
+
 # ssh-agent can remember your passwords
+ssh-add  # will add default locations for keys
 
 # copy current users public key to authorized keys in remoteserver, now you can public/private key auth instead of password
 ssh-copy-id foouser@fooserver
@@ -9,3 +12,5 @@ ssh-copy-id foouser@fooserver
 # location: ~/.ssh/config
 # can turn on AddKeysToAgent, UseKeychain (for osx)
 # can include other config files
+
+# ssh multihop: http://sshmenu.sourceforge.net/articles/transparent-mulithop.html
