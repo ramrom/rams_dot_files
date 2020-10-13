be50d909db889e6ffcb5f94d8fea610991c0498b
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2020-10-12 20:19:57 -0500

    tmux status gpu mem usage color

--- tmux_status_bar.sh
+++ tmux_status_bar.sh
@@ -18,26 +18,31 @@ counter=$(tmux show -v @status-bar-interval-counter)
 tmux set -q @status-bar-interval-counter $(( ($counter + 1) % 2520 ))
 
 # foo=$(tmux_render_timer_bar eggs)
+cpu_legend_color="fg=colour242"
 cpu=$(skip_verify=1 tmux_percent_usage_color $(uptime_loadave) "uptimeload")
-status_line1="Uptime 1min:$cpu"
+status_line1="#[$cpu_legend_color]Uptime 1min:#[default]$cpu"
 if [ "$(uname)" == "Linux" ]; then
     s=$(sensors)
+
     cpufan=$(echo "$s" | grep -E "CPU Fan" | awk '{print $3;}') # | grep --color=never -Eoi '[0-9]+.[0-9]+'
     cputemp=$(echo "$s" | grep -E "CPU Temperature" | awk '{print $3;}')
     cputemp_num=$(echo $cputemp | sed 's/+//g' | grep --color=never -Eo '^[0-9]+')
     cputemp_color=$(tmux_temp_color $cputemp_num)
-    status_line1="$status_line1  CPU-Temp: #[fg=green]$cputemp_color#[default]C CPU-Fan: #[fg=green]$cpufan#[default] |"
+    status_line1="$status_line1 #[$cpu_legend_color]CPU-Temp:#[default] #[fg=green]$cputemp_color#[default]C"
+    status_line1="$status_line1 #[$cpu_legend_color]CPU-Fan:#[default] #[fg=green]$cpufan#[default] |"
 
     # GPU stats
     stats=$(linux_nvidia)
     gpu_temp=$(echo $stats | awk '{print $3}' | sed 's/.$//')
     gputemp_color=$(tmux_temp_color $gpu_temp)
 
-    gpu_mem=$(echo $stats | awk '{print $9 $10 $11}')
+    # gpu_mem=$(echo $stats | awk '{print $9 $10 $11}')     # simple and no coloring
+    gpu_mem_total=$(echo $stats | awk '{print $11}' | sed 's/...$//g')  # TODO: total never changes, memoize this somehow
+    gpu_mem_total_col="#[fg=colour003]$gpu_mem_total#[default]"
     gpu_mem_used=$(echo $stats | awk '{print $9}' | sed 's/...$//g')
-    gpu_mem_used_col="#[fg=colour211]$gpu_mem_used#[default]"
-    gpu_mem_total=$(echo $stats | awk '{print $11}' | sed 's/...$//g')
-    gpu_mem_total_col="#[fg=colour111]$gpu_mem_total#[default]"
+    gpu_mem_usg=$(($gpu_mem_used * 100 / $gpu_mem_total))
+    gpu_col=040; [ "$gpu_mem_usg" -gt 50 ] && gpu_col=208; [ "$gpu_mem_usg" -gt 90 ] && gpu_col=196
+    gpu_mem_used_col="#[fg=colour$gpu_col]$gpu_mem_used#[default]"
     gpu_mem="$gpu_mem_used_col / $gpu_mem_total_col"
 
     gpu_fan=$(echo $stats | awk '{print $2}' | sed 's/.$//g')
@@ -46,11 +51,11 @@ if [ "$(uname)" == "Linux" ]; then
     gpu_util=$(echo $stats | awk '{print $13}' | sed 's/.$//')
     gpu_util_color=$(tmux_percent_usage_color $gpu_util "gpu util")
 
-    gpu_legend_color=colour058
-    status_line1="$status_line1 #[fg=$gpu_legend_color]GPU-Temp:#[default] #[fg=green]$gputemp_color#[default]C"
-    status_line1="$status_line1 #[fg=$gpu_legend_color]GPU-Util:#[default]$gpu_util_color"
-    status_line1="$status_line1 #[fg=$gpu_legend_color]GPU-Mem(MiB):#[default] $gpu_mem"
-    status_line1="$status_line1 #[fg=$gpu_legend_color]GPU-Fan:#[default]$gpu_fan_color"
+    gpu_legend_color="fg=colour058"
+    status_line1="$status_line1 #[$gpu_legend_color]GPU-Temp:#[default] #[fg=green]$gputemp_color#[default]C"
+    status_line1="$status_line1 #[$gpu_legend_color]GPU-Util:#[default]$gpu_util_color"
+    status_line1="$status_line1 #[$gpu_legend_color]GPU-Mem(MiB):#[default] $gpu_mem"
+    status_line1="$status_line1 #[$gpu_legend_color]GPU-Fan:#[default]$gpu_fan_color"
 fi
 tmux set status-format[1] "$status_line1"
 
