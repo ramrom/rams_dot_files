6199f397b1c8427e27a49f64854df5ebd9036cff
Sreeram <sreeram.mittapalli@gmail.com>
2021-02-28 14:29:48 -0600

    cheats

--- shell_functions.sh
+++ shell_functions.sh
@@ -185,6 +185,7 @@ function fk() {
       --height=50% --layout=reverse
 }
 
+# like cd **, but with tree preview
 function fcd() {
     cd $(fd --type d --hidden --exclude .git '.*' $1 | fzf --preview "tree -C {} | head -40")
 }
