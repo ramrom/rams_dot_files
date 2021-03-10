b508b38c78c8c2a9b2cb372d7d68a2641ab66940
Sreeram <sreeram.mittapalli@gmail.com>
2021-03-10 09:47:17 -0600

    cd to dirs in rams tmux env sh

--- scripts/ram_tmux_env.sh
+++ scripts/ram_tmux_env.sh
@@ -3,21 +3,22 @@
 # horsize=`tput cols`    # how to obtain width/columns of terminal window/pane
 # vertsize=`tput lines`   # how to obtain height/rows of terminal window/pane
 SESSION_NAME=ram
+DOTFILE=~/rams_dot_files
 GDRIVE=~/"Google Drive"
 
 tmux has-session -t $SESSION_NAME
 [ $? == 0 ] && echo "$(tput setaf 1) $SESSION_NAME session already exists!" && exit 1
 
-tmux new-session -d -s $SESSION_NAME
+tmux new-session -d -s $SESSION_NAME -c "$GDRIVE"
 tmux select-pane -T 'admin'
 tmux split-window -h -c "$GDRIVE"
 tmux select-pane -T 'admin'
 
-tmux new-window -t $SESSION_NAME:1 -n 'admin' -c "$GDRIVE"
-tmux split-window -h
+tmux new-window -t $SESSION_NAME:1 -n 'admin' -c "$DOTFILE"
+tmux split-window -h -c "$DOTFILE"
 tmux select-pane -T 'admin'
 
-tmux new-window -t $SESSION_NAME:2 -n 'admin' -c "$GDRIVE"
+tmux new-window -t $SESSION_NAME:2 -n 'admin'
 tmux split-window -h
 tmux select-pane -T 'admin'
 
