3d98fd5ac3deb426abdc5af988415d7dd3194833
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2021-03-11 16:44:21 -0600

    tmux-status script, move 3 funcs into it

--- scripts/check_wifi.sh
+++ scripts/check_wifi.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # TODO: make faster, hyperfine has it run in 30ms
-function get_wifi_ssid() {
+get_wifi_ssid () {
     if [ "$(uname)" = "Darwin" ]; then
         local wifi_info=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I)
         echo "$wifi_info" | awk -F: '/ SSID/{print $2}' | sed 's/ //g'
