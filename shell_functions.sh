32172c6ae7c713ae849e28792aa47044fd52cd55
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2021-11-18 16:11:54 -0600

    vil alias uses vin

--- shell_functions.sh
+++ shell_functions.sh
@@ -151,8 +151,7 @@ function binlink() {
     ln -s "$fullpath" ~/bin/$(basename "$1")
 }
 
-function vil() { vi -p $(cat $1); }
-function viln() { vin -p $(cat $1); }
+function vil() { vin -p $(cat $1); }
 
 function fdisk_find() {
     [ -z "$1" ] && echo "arg for diskname needed!" && return 1
