13e309a4439fcadb8e493aa5e11ebda0fab55060
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2023-05-31 18:08:00 -0500

    init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- NEOVIM CONFIG
+
 -- ISSUES
---- 1. fzf is not loading, dont see commands
+    -- fzf is not loading, dont see commands
+    -- cant get leader y/p to copy/paste to + buffer
 
 -------- PLUGINS -------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -23,12 +25,63 @@ require("lazy").setup({
     'joshdick/onedark.vim', { 'joshdick/onedark.vim' },
 })
 
--------- SETTINGS -----------
+--------------------------------------------------------------------------------------------------------
+-------------------------------- SETTINGS --------------------------------------------------------------
+----------------------------------- -------------------------------------------------------------------
+vim.opt.backup = false                 -- no backup files
+vim.opt.writebackup = false            -- only in case you don't want a backup file while editing
+vim.opt.swapfile = false               -- no swap files
+
+vim.opt.mouse=null                     -- turn off all mouse support by default
+
+----- COLOR -----
 vim.g.onedark_termcolors=256
 vim.g.onedark_terminal_italics=1
 vim.cmd('autocmd ColorScheme onedark call onedark#extend_highlight("Normal", { "bg": { "cterm": "000" } })')
 vim.cmd.colorscheme('onedark')
 vim.cmd.highlight({'clear','Search'})
+
+vim.opt.autoread = true                         -- reload file's buffer if file was changed externally
+vim.opt.splitbelow = true                       -- open new windows on bottom for horizontal, right for vertical
+vim.opt.splitright = true                       -- open new windows on bottom for horizontal, right for vertical
+vim.opt.wildmenu = true                         -- display command line's tab complete options as menu
+vim.opt.wildmode = {'longest', 'list', 'full'}  -- complete longest common, list all matches, complete till next full match
+vim.opt.wrap = true                             -- wrap lines longer than width to next line
+vim.opt.linebreak = true                        -- avoid wrapping line in middle of a word
+vim.opt.scrolloff = 1                           -- always show at least one line above or below the cursor
+vim.opt.showcmd = true                          -- show commands i'm process of typing in status bar
+vim.opt.number = true                           -- line numbers
+vim.opt.backspace= {'indent' ,'eol', 'start'}   -- backspace like most wordprocessors in insert mode
+vim.opt.display:append('lastline')              -- display lastline even if its super long
+vim.opt.formatoptions:append('j')               -- Delete comment character when joining commented lines
+
+---- SERACHING
+vim.opt.hlsearch = true                         -- highlight search
+vim.cmd.highlight({'Search','cterm=italic,underline,inverse'})
+vim.opt.incsearch = true               -- searching as you type (before hitting enter)
+vim.opt.ignorecase = true              -- case-insensitive searches
+vim.opt.smartcase = true               -- with ignorecase, search with all lowercase means INsensitive, any uppercase means sensitive
+
+--- FOLDING
+vim.opt.foldmethod = 'indent'
+vim.opt.foldenable = false            -- dont fold everything when opening buffers
+
+-- INDENTING
+vim.opt.autoindent = true       -- indent on new line for inner scopes in code
+vim.opt.shiftwidth=4            -- use 4 spaces for autoindent (cindent)
+vim.opt.tabstop=4               -- space 4 columns when reading a <tab> char in file
+vim.opt.softtabstop=4           -- complicated, see docs
+vim.opt.expandtab = true        -- use spaces when tab is pressed
+
+--- DEFAULT STATUS LINE
+vim.opt.ls=2                    -- line status, two lines for status and command
+vim.opt.statusline=[[ %F%m%r%h%w\ [TYPE=%Y]\ [ASCII=\%03.3b]\ [POS=%04l,%04v][%p%%]\ ]]
+
+
+--- TRAILING SPACES
+vim.opt.list = true
+vim.opt.listchars={tab = '_', trail = '.'}
+
 
 ------- FUNCS -----------------
 -- if there is one tab, move forward buffer, otherwise forward tabs
@@ -43,7 +96,7 @@ TabBufNavBackward = function()
     if #tabinfo == 1 then vim.cmd(':bp!') else vim.cmd(':tabprevious') end
 end
 
--- close tabs and windows if more than one of either, otherwise closes buffers until none and then quit vim
+-- close tabs and windows if more than one of either, otherwise closes buffers until one left and then quit vim
 TabBufQuit = function()
     local tabinfo = vim.fn.gettabinfo()
     if #tabinfo == 1 and #tabinfo[1]['windows'] == 1 then
@@ -53,6 +106,25 @@ TabBufQuit = function()
     end
 end
 
+local Lua = {}
+function Lua.moduleExists(name)
+    if package.loaded[name] then
+        return true
+    else
+        -- Package.Searchers was renamed from Loaders in lua5.2, have support for both
+        ---@diagnostic disable-next-line: deprecated
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                -- luacheck: ignore
+                -- luacheck complains about package.preload being read-only
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
+    end
+end
 ------- MAPS -----------------------
 vim.g.mapleader = " "
 
@@ -64,3 +136,91 @@ vim.keymap.set("n", "<leader>t", "<cmd>:tabnew<CR>")
 vim.keymap.set("n", "<leader>z", function() print("hi") end)
 vim.keymap.set("n", "<leader>q", TabBufQuit)
 vim.keymap.set("n", "<leader>Q", "<cmd>:q!<CR>")
+vim.keymap.set("n", "<leader><leader>q", "<cmd>:qa<CR>")
+vim.keymap.set("n", "<leader>e", "<cmd>:Explore<CR>")
+vim.keymap.set("n", "<leader>y", "\"+y")
+-- vim.keymap.set("n", "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>p", [["+p]])
+
+----------- PLUGIN CONFIG ------------------------------
+if vim.fn.has('nvim-0.8.0') == 1 then
+
+if Lua.moduleExists('lualine') then
+    require('lualine').setup {
+        options = {
+            icons_enabled = false,
+            {'branch', icon = 'ᛘ'},
+            theme = 'onedark',
+            component_separators = { left = '', right = ''},
+            section_separators = { left = '', right = ''},
+            disabled_filetypes = {
+                statusline = {},
+                winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+                statusline = 1000,
+                tabline = 1000,
+                winbar = 1000,
+            }
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            -- lualine_b = { { 'branch', icons_enabled = true, {'branch', icon = 'ᛘ'} }, 'diff', 'diagnostics' },
+            lualine_c = { { 'filename', file_status = true, path = 1 } },
+            lualine_x = {'filetype', 'encoding', 'fileformat'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {
+            lualine_a = {
+                {
+                    'tabs',
+                    mode = 2,
+                    max_length = vim.o.columns,
+
+                    fmt = function(name, context)
+                        -- Show + if buffer is modified in tab
+                        local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                        local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                        local bufnr = buflist[winnr]
+                        local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+                        return name .. (mod == 1 and ' +' or '')
+                    end
+                }
+            },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {
+                {
+                    'buffers',
+                    show_modified_status = true,
+                    mode = 4,
+                    buffers_color = {
+                        inactive = { fg = 'grey', bg = 'black' },
+                        active = 'grey',
+                    },
+                    },
+                },
+        },
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+    }
+end
+
+end
