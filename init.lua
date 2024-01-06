6cfa448edd4c1a11a33710575bfd27533514fcef
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2024-01-06 16:49:29 -0600

    remove veda schema and fveda

--- init.lua
+++ init.lua
@@ -887,7 +887,7 @@ LoadScalaMetals = function()
     metals_config.settings = {
       -- jan'24 default metals version was 0.11.x
       --    see https://scalameta.org/metals/docs/editors/vim/  , 1.2.0 supported
-      serverVersion = "1.2.0",   
+      serverVersion = "1.2.0",
       showImplicitArguments = true,
       showImplicitConversionsAndClasses = true,
       showInferredType = true
