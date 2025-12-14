2a752e41d9866474cc7848a3e450fc158b9cfe60
Sreeram Mittapalli <sreeram.mittapalli@gmail.com>
2025-12-14 09:36:57 -0600

    my public ip alias that hits ipify.org

--- shell_aliases.sh
+++ shell_aliases.sh
@@ -36,6 +36,7 @@ alias batt='bat --color never -pp'  # no color, -pp is plain (no header or line 
 # XH
 alias xhv='xh -v'
 alias xhcn='xhv --verify no https://api.chucknorris.io/jokes/random'  # need no-verify when on work vpn
+alias my_public_ip='xhv https://api.ipify.org format==json'
 
 # SHELL STUFF
 alias raf='source ~/rams_dot_files/shell_aliases.sh && source ~/rams_dot_files/shell_functions.sh'
